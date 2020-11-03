e3151708519b23a188039335affa863373f6b6b9
Nicholas Doty <doty.nick@gmail.com>
2020-11-02 23:33:26 -0500

    Setting config back to pre docker-compose configurations

--- web-app/src/config.js
+++ web-app/src/config.js
@@ -1,3 +1,3 @@
-const MIXER_BASE_URL='http://flask-app:5000';
+const MIXER_BASE_URL='http://localhost:5000';
 export const MIXER_REQUEST_MIX_URL = `${MIXER_BASE_URL}/mix_coins`;
 export const MIXER_CHECK_DEPOSIT_URL = `${MIXER_BASE_URL}/check_deposit`;
